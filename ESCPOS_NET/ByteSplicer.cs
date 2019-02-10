cb32e739496fc98a9f11a09373e5f19d5fae998b
Luke Paireepinart <lukepdev@live.com>
2019-02-10 16:02:35 -0600

    - Add ByteSplicer helper that can concatenate byte arrays

---
+++ ESCPOS_NET/ByteSplicer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ESCPOS_NET.Utilities
+{
+    public static class ByteSplicer
+    {
+        public static byte[] Combine(params object[] byteArrays)
+        {
+            ByteArrayBuilder builder = new ByteArrayBuilder();
+            foreach (var byteArray in byteArrays)
+            {
+                if (!(byteArray is byte[])) throw new ArgumentException("All passed in objects must be byte arrays.");
+                builder.Append((byte[])byteArray);
+            }
+            return builder.ToArray();
+
+        }
+    }
+}
