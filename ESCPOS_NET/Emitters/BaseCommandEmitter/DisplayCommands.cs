37f31df02b2af6c1e9066c99a00ad3cbb25d13a2
Luke Paireepinart <lukepdev@live.com>
2019-12-26 15:38:03 -0800

    #17 - Add support for Clear command for using Epson displays.  ESC @ (Initialize) already exists, so this plus clear plus using normal Print commands should be sufficient for writing to displays.

---
+++ ESCPOS_NET/Emitters/BaseCommandEmitter/DisplayCommands.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ESCPOS_NET.Emitters
+{
+    public abstract partial class BaseCommandEmitter : ICommandEmitter
+    {
+        /* Display Commands */
+        public byte[] Clear() => new byte[] { Display.CLR };
+    }
+}
