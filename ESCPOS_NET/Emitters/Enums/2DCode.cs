c97aef90e8855bb56757338c79f2e557c0e7a8bd
Luke Paireepinart <lukepdev@live.com>
2020-08-02 18:58:15 -0500

    - Add back in whitespace and specifically allow the whitespace for enums in 2DCode module (makes it easier to read)

--- ESCPOS_NET/Emitters/Enums/2DCode.cs
+++ ESCPOS_NET/Emitters/Enums/2DCode.cs
@@ -1,27 +1,30 @@
 ﻿namespace ESCPOS_NET.Emitters
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1025:Code should not contain multiple whitespace in a row", Justification = "Enums are easier to read if they have whitespace alignment.")]
     public enum TwoDimensionCodeType
     {
-        PDF417 = 0,
+        PDF417        = 0,
         QRCODE_MODEL1 = 49,
-        QRCODE_MODEL2,
-        QRCODE_MICRO,
+        QRCODE_MODEL2 = 50,
+        QRCODE_MICRO  = 51,
     }
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1025:Code should not contain multiple whitespace in a row", Justification = "Enums are easier to read if they have whitespace alignment.")]
     public enum Size2DCode
     {
-        TINY = 2,
-        SMALL,
-        NORMAL,
-        LARGE,
-        EXTRA,
+        TINY   = 2,
+        SMALL  = 3,
+        NORMAL = 4,
+        LARGE  = 5,
+        EXTRA  = 6,
     }
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1025:Code should not contain multiple whitespace in a row", Justification = "Enums are easier to read if they have whitespace alignment.")]
     public enum CorrectionLevel2DCode
     {
-        PERCENT_7 = 48,
-        PERCENT_15,
-        PERCENT_25,
-        PERCENT_30,
+        PERCENT_7  = 48,
+        PERCENT_15 = 49,
+        PERCENT_25 = 50,
+        PERCENT_30 = 51,
     }
 }
