6553775f6292b35f4d545efc2282d71357bc81f0
Igor campos <igorocampos@gmail.com>
2020-09-30 17:07:29 -0300

    better performance of ByteSplicer

--- ESCPOS_NET/ByteSplicer.cs
+++ ESCPOS_NET/ByteSplicer.cs
@@ -1,22 +1,14 @@
-﻿using System;
-
-namespace ESCPOS_NET.Utilities
+﻿namespace ESCPOS_NET.Utilities
 {
     public static class ByteSplicer
     {
-        public static byte[] Combine(params object[] byteArrays)
+        public static byte[] Combine(params byte[][] byteArrays)
         {
             ByteArrayBuilder builder = new ByteArrayBuilder();
             foreach (var byteArray in byteArrays)
             {
-                if (!(byteArray is byte[]))
-                {
-                    throw new ArgumentException("All passed in objects must be byte arrays.");
-                }
-
-                builder.Append((byte[])byteArray);
+                builder.Append(byteArray);
             }
-
             return builder.ToArray();
         }
     }
