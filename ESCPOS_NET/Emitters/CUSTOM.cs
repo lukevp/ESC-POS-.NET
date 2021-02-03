53d98ecc6fb83ea8bb7992499c87557b5eb38022
Jonathan Malda <jmalda@nuway.local>
2021-02-03 14:09:06 -0500

    Initial implementation framework for CUSTOM printers, with overridden support for QR Code printing.

---
+++ ESCPOS_NET/Emitters/CUSTOM.cs
@@ -0,0 +1,10 @@
+﻿namespace ESCPOS_NET.Emitters
+{
+    /// <summary>
+    /// CUSTOM Printer (https://www.custom.biz/) implementation of the ESC \ POS protocol. The majority of the commands are identical. However, a small 
+    /// number of them are unique to the CUSTOM implementation.
+    /// </summary>
+    public class CUSTOM : CustomCommandEmitter
+    {
+    }
+}
---
+++ ESCPOS_NET/Emitters/CustomCommandValues/Barcodes.cs
@@ -0,0 +1,15 @@
+﻿namespace ESCPOS_NET.Emitters.CustomCommandValues
+{
+    public static class Barcodes
+    {
+        /// <summary>
+        /// CUSTOM implements the Store QR Code Data slightly differently than the base implementation
+        /// </summary>
+        public static readonly byte[] StoreQRCodeData = { 0x31, 0x50, 0x31 };
+
+        /// <summary>
+        /// CUSTOM implements the Print QR Code slightly differently than the base implementation
+        /// </summary>
+        public static readonly byte[] PrintQRCode = { 0x03, 0x00, 0x31, 0x51, 0x31 };
+    }
+}
