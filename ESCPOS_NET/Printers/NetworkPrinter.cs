daf28715e856f98d1394b15a63ef058eb5c8d8ef
CandL Development <hello@candldev.com>
2021-07-19 20:31:54 -0700

    :art: Remove commented/unused code

--- ESCPOS_NET/Printers/NetworkPrinter.cs
+++ ESCPOS_NET/Printers/NetworkPrinter.cs
@@ -41,70 +41,6 @@ namespace ESCPOS_NET
             Task.Run(() => Connect());
         }
 
-        //protected override void Reconnect()
-        //{
-        //    if (_settings == null) return;
-        //    if (!_settings.ReconnectOnTimeout)
-        //    {
-        //        Logging.Logger?.LogInformation($"[{PrinterName}] Reconnect: Settings have disabled reconnection, skipping reconnect attempt.");
-        //        return;
-        //    }
-
-        //    reconnectAttempts += 1;
-        //    Logging.Logger?.LogTrace($"[{PrinterName}] Reconnect: Reconnection attempt {reconnectAttempts}.");
-        //    // TODO: implement simple TCP
-        //    try
-        //    {
-        //        Writer?.Flush();
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Logging.Logger?.LogDebug(e, $"[{PrinterName}] Reconnect: Issue flushing writer.");
-        //    }
-        //    try
-        //    {
-        //        Writer?.Close();
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Logging.Logger?.LogDebug(e, $"[{PrinterName}] Reconnect: Issue closing writer.");
-        //    }
-        //    try
-        //    {
-        //        Reader?.Close();
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Logging.Logger?.LogDebug(e, $"[{PrinterName}] Reconnect: Issue closing reader.");
-        //    }
-        //    try
-        //    {
-        //        _sockStream?.Close();
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Logging.Logger?.LogDebug(e, $"[{PrinterName}] Reconnect: Issue closing socket stream.");
-        //    }
-        //    try
-        //    {
-        //        _socket?.Close();
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Logging.Logger?.LogDebug(e, $"[{PrinterName}] Reconnect: Issue closing socket.");
-        //    }
-        //    try
-        //    {
-        //        Logging.Logger?.LogDebug($"[{PrinterName}] Reconnect: Attempting to reconnect...");
-        //        Connect();
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Logging.Logger?.LogError(e, $"[{PrinterName}] Reconnect: Failed to reconnect.");
-        //        throw;
-        //    }
-        //}
-
         private void Connected(object sender, ClientConnectedEventArgs e)
         {
             _isConnected = true;
@@ -114,10 +50,6 @@ namespace ESCPOS_NET
             _isConnected = false;
             // Invoke reconnect attempt in a background thread so we don't block the event handler thread.
             Task.Run(() => { AttemptReconnectInfinitely(); });
-
-        }
-        private void DataReceived(object sender, DataReceivedEventArgs e)
-        {
 
         }
         private void AttemptReconnectInfinitely()
@@ -147,14 +79,6 @@ namespace ESCPOS_NET
 
             AttemptReconnectInfinitely();
 
-            // let's go!
-            // Attempt to connect for 5 minutes. If it fails, try again.
-            // TODO: adapt binaryreader and binarywriter to the SimpleClient so that when 
-            // DataReceived is fired or when write is called it will send it to the client lib (SendAsync)
-
-            //// Need to review the parameters set here
-            //Writer = new BinaryWriter(_sockStream, new UTF8Encoding(), true);
-            //Reader = new BinaryReader(_sockStream, new UTF8Encoding(), true);
         }
 
         protected override void OverridableDispose()
