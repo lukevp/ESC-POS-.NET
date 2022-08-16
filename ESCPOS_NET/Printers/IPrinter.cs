bb33fe3499aed0007086b81ea6fb48191335e4d7
Luke Paireepinart <luke@paireepinart.com>
2022-08-15 18:33:40 -0700

    Deprecate events from the IPrinter interface
    These events aren't generic enough and shouldn't have been on the IPrinter interface

--- ESCPOS_NET/Printers/IPrinter.cs
+++ ESCPOS_NET/Printers/IPrinter.cs
@@ -7,11 +7,5 @@ namespace ESCPOS_NET
         PrinterStatusEventArgs GetStatus();
         void Write(params byte[][] arrays);
         void Write(byte[] bytes);
-        event EventHandler StatusChanged;
-        event EventHandler Disconnected;
-        event EventHandler Connected;
-        //event EventHandler WriteFailed;
-        //event EventHandler Idle;
-        //event EventHandler IdleDisconnected; is this useful? to know that it disconnected because of idle? probably better to have this as info in disconnected event object instead.
     }
 }
