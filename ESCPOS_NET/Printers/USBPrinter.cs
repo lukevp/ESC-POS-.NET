59079872af9fa86df9b4fda6a9f88a4054ce2064
Gary-gr9 <gary@posmate.co.nz>
2023-07-06 17:16:36 +1200

    Implemented native USB Printing support for Windows (should work for linux as well) with complete printer status report back. No driver install or Virtual Serial Port needed. Choose printer from list of usb devices and send data straight to the printer like Serial or Network printer. Possible fix for Isuues : #186 #220 #223

---
+++ ESCPOS_NET/Printers/USBPrinter.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace ESCPOS_NET.Printers
+{
+    public class USBPrinter : BasePrinter
+    {
+        private readonly FileStream _rfile;
+        private readonly FileStream _wfile;
+        public USBPrinter(string usbPath)
+            : base()
+        {
+            //keeping separate file streams performs better
+            //while using 1 file stream printers were having intermittent issues while printing
+            //your milege may vary
+            _rfile = File.Open(usbPath, FileMode.Open, FileAccess.Read);
+            _wfile = File.Open(usbPath, FileMode.Open, FileAccess.Write);
+            Writer = new BinaryWriter(_wfile);
+            Reader = new BinaryReader(_rfile);
+        }
+
+        ~USBPrinter()
+        {
+            Dispose(false);
+        }
+
+        protected override void OverridableDispose()
+        {
+            _rfile?.Close();
+            _rfile?.Dispose();
+            _wfile?.Close();
+            _wfile?.Dispose();
+        }
+    }
+}
